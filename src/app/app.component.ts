673606eb86b1f5bd161b770342da0ae8b2da9cca
Marten Mark <martenmark.it@gmail.com>
2024-05-16 19:46:07 +0300

    removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { UserFormFeature } from './features/user-form/user-form.feature';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, UserFormFeature],
+  imports: [UserFormFeature],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
